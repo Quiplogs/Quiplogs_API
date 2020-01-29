81ab2e53477e05e803613a2b8dcc3c7182754656
jonathan@betabit.co.za <Jonathan@betabit.co.za>
2020-01-29 23:40:22 +0200

    Updated PagedResult to be used via the core module

---
+++ Api.Core/Domain/PagedResult.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Core.Domain
+{
+    public class PagedResult<T>
+    {
+        public Pager Pager { get; set; }
+
+        public List<T> PagedItems { get; set; }
+
+        /// <summary>
+        /// Gets a Paged List Result as needed by the Angular Pager
+        /// </summary>
+        /// <param name="items">List of Items where Skip() and Take() has alreadsy been applied</param>
+        /// <param name="totalItems">Total number of items in the DB</param>
+        /// <param name="currentPage">Current pager from the pager</param>
+        public PagedResult(List<T> items, int totalItems, int currentPage, int pageSize)
+        {
+            var totalPages = Math.Ceiling((double)totalItems / pageSize);
+            var startIndex = (currentPage * pageSize) - pageSize;
+            var endIndex = (currentPage * pageSize) - 1;
+
+            int[] pages = new int[] { currentPage - 1, currentPage, currentPage + 1 };
+            if (currentPage == 1)
+            {
+                pages = new int[] { 1 };
+                if (totalPages == 2)
+                {
+                    pages = new int[] { 1, 2 };
+                }
+                if(totalPages > 2)
+                {
+                    pages = new int[] { 1, 2, 3 };
+                }
+            }
+
+            if(currentPage == totalPages)
+            {
+                pages = new int[] { currentPage - 2, currentPage - 1, currentPage };
+            }
+
+            Pager = new Pager(totalItems, currentPage, pageSize, totalPages, startIndex, endIndex, pages);
+            PagedItems = items;
+        }
+    }
+}
